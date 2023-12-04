d7301680651d1588232db9e9648cf05775cd1c17
ddlpr <103121607+ddlpr@users.noreply.github.com>
2023-12-04 13:52:46 -0800

    Use built-in weather icons from API
    Refactor API code into weather.js and UI logic into app.js.
    Add min and max temp icons as well as humidity level icon.

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,74 @@
 'use strict';
+
+const weatherFormButtonElement = document.querySelector('.weatherForm__btn');
+const weatherFormInputElement = document.querySelector('.weatherForm__input');
+const weatherCardElement = document.querySelector('.weatherCard');
+const weatherCardImageElement = document.querySelector('.weatherCard__img');
+const weatherCardTimeElement = document.querySelector('.weatherCard__time');
+const weatherCardCityElement = document.querySelector('.weatherCard__city');
+const weatherCardTempElement = document.querySelector('.weatherCard__temp');
+const weatherCardDescElement = document.querySelector('.weatherCard__desc');
+const weatherCardHumidityElement = document.querySelector('.weatherCard__humidity');
+const weatherCardMinTempElement = document.querySelector('.weatherCard__temp-min');
+const weatherCardMaxTempElement = document.querySelector('.weatherCard__temp-max');
+const errorElement = document.querySelector('.error');
+const errorCityElement = document.querySelector('.error__city');
+
+const parseCityName = function (cityName) {
+  return cityName
+    .split(' ')
+    .map(word => word[0].toUpperCase() + word.slice(1))
+    .join(' ');
+};
+
+const displayWeather = function (data, city) {
+  if (data.cod === '404') {
+    weatherCardElement.classList.add('hidden');
+    errorCityElement.textContent = parseCityName(city);
+    errorElement.classList.remove('hidden');
+  } else {
+    const temperature = data.main.temp;
+    const minTemp = data.main.temp_min;
+    const maxTemp = data.main.temp_max;
+    const description = data.weather[0].description;
+    const humidity = data.main.humidity;
+    const country = data.sys.country;
+
+    console.log(data);
+    const currentDate = new Date();
+    const localTimeISOString = currentDate.toISOString();
+    const startIndex = localTimeISOString.indexOf('T') + 1;
+    const endIndex = localTimeISOString.indexOf('.');
+    const localTime = localTimeISOString.slice(startIndex, endIndex);
+    let hour = (Number(localTime.slice(0, 2)) + data.timezone / 60 / 60);
+    hour += hour < 0 ? 24 : hour >= 24 ? -24 : 0;
+
+    if (data.weather[0].icon[2] === 'n') weatherCardElement.classList.add('weatherCard--dark')
+    else weatherCardElement.classList.remove('weatherCard--dark');
+
+    weatherCardImageElement.setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+    weatherCardTempElement.textContent = temperature;
+    weatherCardDescElement.textContent = description.replace(description[0], description[0].toUpperCase());
+    weatherCardHumidityElement.textContent = humidity;
+    weatherCardMinTempElement.textContent = minTemp;
+    weatherCardMaxTempElement.textContent = maxTemp;
+    weatherCardCityElement.textContent = `${parseCityName(city)}, ${country}`;
+    weatherCardTimeElement.textContent = `${hour.toString().padStart(2, '0')}${localTime.slice(2)} ${Number(hour) >= 12 ? 'PM' : 'AM'}`;
+
+    weatherCardElement.classList.remove('hidden');
+    errorElement.classList.add('hidden');
+  }
+};
+
+const newSearch = async function (city) {
+  const data = await getWeather(city);
+  return data;
+}
+
+weatherFormButtonElement.addEventListener('click', function () {
+  let city = weatherFormInputElement.value.trim().toLowerCase();
+  
+  newSearch(city)
+    .then(data => displayWeather(data, city));
+  weatherFormInputElement.value = '';
+});
--- scripts/weather.js
+++ scripts/weather.js
@@ -1,81 +1,8 @@
-const weatherFormButtonElement = document.querySelector('.weatherForm__btn');
-const weatherFormInputElement = document.querySelector('.weatherForm__input');
-const weatherCardElement = document.querySelector('.weatherCard');
-const weatherCardImageElement = document.querySelector('.weatherCard__img');
-const weatherCardTimeElement = document.querySelector('.weatherCard__time');
-const weatherCardCityElement = document.querySelector('.weatherCard__city');
-const weatherCardTempElement = document.querySelector('.weatherCard__temp');
-const weatherCardDescElement = document.querySelector('.weatherCard__desc');
-const weatherCardHumidityElement = document.querySelector('.weatherCard__humidity');
-const weatherCardMinTempElement = document.querySelector('.weatherCard__temp-min');
-const weatherCardMaxTempElement = document.querySelector('.weatherCard__temp-max');
-
-const errorElement = document.querySelector('.error');
-const errorCityElement = document.querySelector('.error__city');
-
-const parseCityName = function (cityName) {
-  return cityName
-    .split(' ')
-    .map(word => word[0].toUpperCase() + word.slice(1))
-    .join(' ');
-};
-
-const displayWeather = function (data, city) {
-  if (data.cod === '404') {
-    weatherCardElement.classList.add('hidden');
-    errorCityElement.textContent = parseCityName(city);
-    errorElement.classList.remove('hidden');
-  } else {
-    const temperature = data.main.temp;
-    const minTemp = data.main.temp_min;
-    const maxTemp = data.main.temp_max;
-    const description = data.weather[0].description;
-    const humidity = data.main.humidity;
-    const country = data.sys.country;
-
-    console.log(data);
-    const currentDate = new Date();
-    const localTimeISOString = currentDate.toISOString();
-    const startIndex = localTimeISOString.indexOf('T') + 1;
-    const endIndex = localTimeISOString.indexOf('.');
-    const localTime = localTimeISOString.slice(startIndex, endIndex);
-    let hour = (Number(localTime.slice(0, 2)) + data.timezone / 60 / 60);
-    if (hour < 0) hour += 24;
-    if (hour >= 20 || hour <= 5) {
-      weatherCardImageElement.textContent = '🌙';
-      weatherCardElement.classList.add('weatherCard--dark');
-    } else {
-      weatherCardImageElement.textContent = '☀️';
-      weatherCardElement.classList.remove('weatherCard--dark');
-    }
-
-    weatherCardTempElement.textContent = temperature;
-    weatherCardDescElement.textContent = description.replace(description[0], description[0].toUpperCase());
-    weatherCardHumidityElement.textContent = humidity;
-    weatherCardMinTempElement.textContent = minTemp;
-    weatherCardMaxTempElement.textContent = maxTemp;
-    weatherCardCityElement.textContent = `${parseCityName(city)}, ${country}`;
-    weatherCardTimeElement.textContent = `${hour.toString().padStart(2, '0')}${localTime.slice(2)} ${Number(hour) >= 12 ? 'PM' : 'AM'}`;
-
-    weatherCardElement.classList.remove('hidden');
-    errorElement.classList.add('hidden');
-  }
-};
-const getWeather = function () {
-  const api_key = '88f58c9268079e3201d24de9c4a44648';
-  let city = weatherFormInputElement.value.toLowerCase();
-
+const api_key = '88f58c9268079e3201d24de9c4a44648';
+const getWeather = async function (city) {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric&lang=es`;
+  const weather = await fetch(url).then(response => response.json());
 
-  fetch(url)
-    .then(response => response.json())
-    .then(data => displayWeather(data, city))
-    .catch(error => {
-      console.error('Algo salió mal', error);
-    });
+  return weather;
 };
 
-weatherFormButtonElement.addEventListener('click', function () {
-  getWeather();
-  weatherFormInputElement.value = '';
-});
